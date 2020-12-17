e6cff72272d0865a6c3691ba0b4cfd382ea672de
Davide De Grazia <s1094615@studenti.univpm.it>
2020-12-17 16:54:15 +0100

    Inizializer

---
+++ ProgettoOOP/src/main/java/com/ddgfm/ProgettoOOP/ProgettoOopApplication.java
@@ -0,0 +1,13 @@
+package com.ddgfm.ProgettoOOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoOopApplication.class, args);
+	}
+
+}
---
+++ ProgettoOOP/src/test/java/com/ddgfm/ProgettoOOP/ProgettoOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddgfm.ProgettoOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
