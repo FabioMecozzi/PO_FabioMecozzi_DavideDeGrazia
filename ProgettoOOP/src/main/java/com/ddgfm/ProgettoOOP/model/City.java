9e3e89feb3689a98c0bebe5336ffc991ef818f31
FabioMecozzi <74898663+FabioMecozzi@users.noreply.github.com>
2021-01-12 11:49:35 +0100

    Aggiornamento modelli City e CityUV
    City è ora abstract
    Eliminata la classe cooord in City
    Migliorata la gestinone dei dati in CityUV

--- ProgettoOOP/src/main/java/com/ddgfm/ProgettoOOP/model/City.java
+++ ProgettoOOP/src/main/java/com/ddgfm/ProgettoOOP/model/City.java
@@ -1,49 +1,99 @@
 package com.ddgfm.ProgettoOOP.model;
 
-public class City {
+import java.io.File;
 
-	private int id;
+import com.ddgfm.ProgettoOOP.utils.ModelUtils;
+
+//Questa classe rappresenta il modello dei dati contenuti nell'archivio.
+//Ci sta solo il problema che bisogna passare per parametri le coordinate,
+//mentre sarebbe preciso se passassimo il nome della città
+
+
+public class City implements FromJson {
+
 	private String name;
-	
-	public City(int id, String name) {
-		
-		this.id = id;
-		this.name = name;
+	private String lat;
+	private String lon;
+	private long id;
+
+	public City() {
+		super();
 	}
-	
-	public int getId() {
-		return id;
+
+	public City(long id, String name, String lon, String lat) throws NumberFormatException {
+		this(name, lon, lat);
+		try {
+			setId(id);
+		} catch (NumberFormatException e) {
+			throw e;
+		}
+	}
+
+	public City(String name, String lon, String lat) throws NumberFormatException {
+		this.name = name;
+		try {
+			setLat(lat);
+			setLon(lon);
+		} catch (NumberFormatException e) {
+			throw e;
+		}
+
+	}
+
+	String getLat() {
+		return lat;
+	}
+
+	void setLat(String lat) throws NumberFormatException {
+		if (ModelUtils.isNumeric(lat)) {
+			this.lat = lat;
+		} else {
+			throw new NumberFormatException();
+		}
+	}
+
+	String getLon() {
+		return lon;
+	}
+
+	void setLon(String lon) {
+		if (ModelUtils.isNumeric(lon)) {
+			this.lon = lon;
+
+		} else {
+			throw new NumberFormatException();
+		}
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	private class Coord {
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		private long lat;
-		private long lon;
+	public long getId() {
+		return id;
+	}
 
-		public Coord(long lat, long lon) {
-			super();
-			this.lat = lat;
-			this.lon = lon;
+	public void setId(long id) {
+		if (id > 0) {
+			this.id = id;
+		} else {
+			throw new NumberFormatException("L'id deve essere positivo");
 		}
+	}
+
+	@Override
+	public void JSONtoJava(File json) {
+		// TODO Auto-generated method stub
+		
+	}
 	
-		
-		public long getLat() {
-			return lat;
-		}
-		public void setLat(long lat) {
-			this.lat = lat;
-		}
-		public long getLon() {
-			return lon;
-		}
-		public void setLon(long lon) {
-			this.lon = lon;
-		}
-		
+	@Override
+	public String toString() {
+		return(""+ "ID: "+ this.id+ " nome: "+ this.name+" Longitudine: "+ this.lon +" Latitudine: "+ this.lat);
 	}
 
 }
--- ProgettoOOP/src/main/java/com/ddgfm/ProgettoOOP/model/CityUV.java
+++ ProgettoOOP/src/main/java/com/ddgfm/ProgettoOOP/model/CityUV.java
@@ -1,93 +1,205 @@
 package com.ddgfm.ProgettoOOP.model;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TreeSet;
+import java.util.LinkedHashMap;
 
-import org.springframework.web.client.RestTemplate;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import net.minidev.json.parser.ParseException;
+import javassist.NotFoundException;
 
-//Questa classe rappresenta il modello dei dati contenuti nell'archivio.
-//Ci sta solo il problema che bisogna passare per parametri le coordinate,
-//mentre sarebbe preciso se passassimo il nome della città
+/**
+ * Classe che rappresenta una città a cui è legato uno storico di valori di uv
+ * 
+ * @author meefa
+ *
+ */
 
-public class CityUV extends City {
+@Entity
+@Table(name="cityUVs")
+public class CityUV extends City implements FromJson {
 
-	private class Uv {
-
-		private Date dataeora;
-		private long uVvalue;
-
-		public Uv(Date dataeora, long uVvalue) {
-			super();
-			this.dataeora = dataeora;
-			this.uVvalue = uVvalue;
-		}
-
+	/**
+	 * Indica se la città deve essere monitorata
+	 */
+	boolean monitored = false;
+	
+	void setGoodForecast(int goodForecast) {
+		this.goodForecast = goodForecast;
+	}
+	void setTotalForecast(int totalForecast) {
+		this.totalForecast = totalForecast;
 	}
 
-	private static double accouracy = 0.0;
-	private static int goodForecast = 0;
-	private static int totalForecast = 0;
+	/**
+	 * Map che contiene i dati dei raggi uv legati al giorno
+	 */
+	private LinkedHashMap<Date, Double> record;
 
-	public static double getAccouracy() {
+	private int goodForecast;
+	private int totalForecast;
+	private double accouracy = -1.0;
+
+	public CityUV(Long ID,String name, String Lon, String Lat) {
+		super(ID, name, Lon, Lat);
+	}
+	public CityUV(City city) {
+		super(city.getId(), city.getName(), city.getLon(), city.getLat() );
+	}
+	
+	@Id
+	public long getId() {
+		return super.getId();
+	}
+
+	public double getAccouracy() {
 		return accouracy;
 	}
 
-	public static void setAccouracy(double accouracy) {
-		CityUV.accouracy = accouracy;
-	}
-
-	public static int getGoodForecast() {
+	public int getGoodForecast() {
 		return goodForecast;
 	}
 
-	public static void setGoodForecast(int goodForecast) {
-		CityUV.goodForecast = goodForecast;
-	}
-
-	public static int getTotalForecast() {
+	public int getTotalForecast() {
 		return totalForecast;
 	}
 
-	public static void setTotalForecast(int totalForecast) {
-		CityUV.totalForecast = totalForecast;
+	public boolean isMonitored() {
+
+		return monitored;
 	}
 
-	private long uVvalue;
-	private Date dataeora;
+	@Override
+	public void JSONtoJava(File json) {
+		// TODO Auto-generated method stub
 
-	public CityUV(int id, String name, Date dataeora, long uVvalue) {
-		super(id, name);
-		this.dataeora = dataeora;
-		this.uVvalue = uVvalue;
 	}
 
-	private TreeSet<Long> uVlist;
+	public void setAccouracy(double accouracy) {
+		this.accouracy = accouracy;
+	}
 
 
-	public void JSONtoJava() {
-		@SuppressWarnings("deprecation")
-		JSONParser parser = new JSONParser();
-		JSONObject obj = null;
-		RestTemplate restTemplate = new RestTemplate();
-		String apirisultato = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/uvi?lat=" + this.lat
-				+ "&lon=" + this.lon + "&appid=58b2755bcc79abb83cce4474b0a795b8", String.class);
-
-		System.out.println(apirisultato);
-
-		try {
-			obj = (JSONObject) parser.parse(apirisultato);
-			// manca il passaggio da nome a coordinate e da coordinate a nome
-
-			this.dataeora = (Date) obj.get("date_iso");
-			this.uVvalue = (long) obj.get("value");
-		} catch (ParseException e) {
-			e.printStackTrace();
+	public void setMonitored(boolean monitored) {
+		if (monitored == true && record == null) {
+			record = new LinkedHashMap<>();
 		}
-
+		this.monitored = monitored;
 	}
 
+
+	/*
+	 * CityUV(File json){ JSONtoJava(json); } //funzione che bisogna
+	 * implementare---->
+	 * 
+	 * @Override public void JSONtoJava(File json) {
+	 * 
+	 * @SuppressWarnings("deprecation") JSONParser parser = new JSONParser();
+	 * JSONObject obj = null; RestTemplate restTemplate = new RestTemplate(); String
+	 * apirisultato =
+	 * restTemplate.getForObject("http://api.openweathermap.org/data/2.5/uvi?lat=" +
+	 * this.lat + "&lon=" + this.lon + "&appid=58b2755bcc79abb83cce4474b0a795b8",
+	 * String.class);
+	 * 
+	 * System.out.println(apirisultato);
+	 * 
+	 * try { obj = (JSONObject) parser.parse(apirisultato); // manca il passaggio da
+	 * nome a coordinate e da coordinate a nome
+	 * 
+	 * this.dataeora = (Date) obj.get("date_iso"); this.uVvalue = (long)
+	 * obj.get("value"); } catch (ParseException e) { e.printStackTrace();
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
+
+	LinkedHashMap<Date, Double> getRecord() {
+		return record;
+	}
+
+	public void setRecord(LinkedHashMap<Date, Double> record) {
+		this.record = record;
+	}
+
+	/**
+	 * 
+	 * @param todayValue    valore dei raggi uv di oggi
+	 * @param tomorrowValue valore della previsione del valore dei raggi uv di
+	 *                      domani
+	 * @param trasholdError perc
+	 * @throws NotFoundException
+	 */
+	public void updateRecord(double todayValue, double tomorrowValue, double trasholdError) throws NotFoundException, IllegalArgumentException {
+		Calendar c = Calendar.getInstance();
+		c.setTime(java.sql.Date.valueOf(LocalDate.now()));
+		c.add(Calendar.DATE, 1);
+		Date tomorrow = c.getTime();
+		this.record.put(tomorrow, todayValue);
+		try {
+			veriryTodayForecast(todayValue, trasholdError);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * 
+	 * @param newCity CityUV contenente i valori da unire con quelli già presenti
+	 */
+	public void updateRecord(CityUV newCityUV, double trasholdError) {
+		Date today = java.sql.Date.valueOf(LocalDate.now());
+		if(newCityUV.getRecord().containsKey(today)) {
+			try {
+				////////da implementare
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+
+	public boolean veriryTodayForecast(double actualValue, double trasholdError)
+			throws NotFoundException, IllegalArgumentException {
+		Date today = java.sql.Date.valueOf(LocalDate.now());
+
+		if (record.containsKey(today)) {
+			try {
+				this.totalForecast++;
+				if (valueIsGood(today, actualValue, trasholdError) == true) {
+					this.goodForecast++;
+					this.accouracy = this.goodForecast / this.totalForecast;
+					return true;
+				}
+				this.accouracy = this.goodForecast / this.totalForecast;
+				return false;
+			} catch (Exception e) {
+				throw e;
+			}
+		} else {
+			throw new NotFoundException("Il registro non contiene il valore di oggi");
+		}
+	}
+
+	private boolean valueIsGood(Date day, double actualValue, double trasholdError) throws IllegalArgumentException {
+		if (trasholdError == 0) {
+			throw new IllegalArgumentException("trasholdError deve essere maggiore di 0");
+		}
+		return ((Math.abs(this.record.get(day) - actualValue) / actualValue) <= trasholdError);
+	}
+	
+	//TODO: da implementare (per tests)
+	@Override
+	public String toString() {
+		String resultString= super.toString()+" monitored: "+ monitored+ " goodForecast: "+ goodForecast + " totalForecast: "+ totalForecast + " accouracy: "+ accouracy + " record: ";
+		try {
+			resultString+= record.toString();
+		} catch (NullPointerException e) {
+			resultString+= "null";
+		}
+		return resultString;
+	}
 }
