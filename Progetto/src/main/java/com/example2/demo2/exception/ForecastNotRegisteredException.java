f717855ec7580bb1f11ee37a7733095999e3ca5f
FabioMecozzi <74898663+FabioMecozzi@users.noreply.github.com>
2021-01-14 20:20:27 +0100

    Testato e fixato CityUV
    Adesso CityUV gestisce autonomamente l'accouracy delle proprie
    previsioni, creata la classe test per CityUV, create 3 classi di
    eccezioni personalizzate per gestire le eccezioni di CityUV

---
+++ Progetto/src/main/java/com/example2/demo2/exception/ForecastNotRegisteredException.java
@@ -0,0 +1,35 @@
+package com.example2.demo2.exception;
+
+public class ForecastNotRegisteredException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ForecastNotRegisteredException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ForecastNotRegisteredException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ForecastNotRegisteredException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ForecastNotRegisteredException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ForecastNotRegisteredException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Progetto/src/main/java/com/example2/demo2/exception/IdNotFoundException.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.example2.demo2.exception;
+
+/**
+ * @author meefa
+ *
+ */
+public class IdNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public IdNotFoundException() {
+		super();
+	}
+	public IdNotFoundException(String message) {
+		super(message);
+	}
+
+}
