d0a6e87532913fcbc2bc708bbdbee247edeaac9f
FabioMecozzi <74898663+FabioMecozzi@users.noreply.github.com>
2021-01-18 19:30:06 +0100

    Eliminata annotazione inutile

--- Progetto/src/main/java/com/example2/demo2/Demo2Application.java
+++ Progetto/src/main/java/com/example2/demo2/Demo2Application.java
@@ -3,9 +3,11 @@ package com.example2.demo2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableScheduling
 public class Demo2Application {
 
 	public static void main(String[] args) {
