b4c0d30c06996170091ab936db05409db2830884
Davide De Grazia <s1094615@studenti.univpm.it>
2021-01-20 18:54:53 +0100

    creata la classe

--- Progetto/src/main/java/com/example2/demo2/utils/CityListParser.java
+++ Progetto/src/main/java/com/example2/demo2/utils/CityListParser.java
@@ -1,22 +1,64 @@
 package com.example2.demo2.utils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import com.example2.demo2.model.City;
 
-public class CityListParser {
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 
-	//Da implementare?
-	public List<City> parseIT(File source) {
-		// TODO Auto-generated method stub
-		return null;
+public class CityListParser { 
+
+	@SuppressWarnings({ "unchecked", "null" })
+	public static List<City> parse(File source) {
+		
+		
+		@SuppressWarnings("unused")
+		JSONObject jsonObject;
+		
+		
+		City city = null ;
+		
+
+		@SuppressWarnings("deprecation")
+		JSONParser parser = new JSONParser();
+		
+		 
+		Object obj = null;
+		try {
+			obj = (JSONObject) parser.parse(new FileReader(source));
+		} catch (FileNotFoundException | ParseException e) {
+		
+			e.printStackTrace();
+		}
+		
+		city.setName((String) ((HashMap<String,Object>) obj).get("name"));
+		city.setCountry((String) ((HashMap<String,Object>) obj).get("country"));
+		JSONObject coord = (JSONObject) ((HashMap<String,Object>) obj).get("coord");
+		city.setLat((String) coord.get("lat"));
+		city.setLon((String) coord.get("lon"));
+
+		
+		
+		LinkedHashSet<City> set = new LinkedHashSet<City>();
+		Iterator<City> it = set.iterator();
+		
+		while (it.hasNext()) {
+			set.add(city);
+			it.next();
+			
+		}
+		
+		return (List<City>) set;
 	}
 
-	public List<City> parse(File source) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 
 }
