d7d119012fcefed20a4359296b34cac379aab21f
davidedegrazia <75479603+davidedegrazia@users.noreply.github.com>
2021-01-21 15:36:38 +0100

    risolti problemi

--- Progetto/src/main/java/com/example2/demo2/utils/CityListParser.java
+++ Progetto/src/main/java/com/example2/demo2/utils/CityListParser.java
@@ -3,70 +3,62 @@ package com.example2.demo2.utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 
-import com.example2.demo2.model.City;
+import com.example2.demo2.model.CityWithCountry;
 
+import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
 
 public class CityListParser { 
 
-	@SuppressWarnings({ "unchecked", "null" })
-	public static List<City> parse(File source) {
+	
+	public static List<CityWithCountry> parse(File source) {
 		
+	   ArrayList<CityWithCountry> cities = new ArrayList<CityWithCountry>();
 		
-		@SuppressWarnings("unused")
-		JSONObject jsonObject;
-		
-		
-		City city = null ;
-		
-
 		@SuppressWarnings("deprecation")
 		JSONParser parser = new JSONParser();
+	
 		
-		 
-		Object obj = null;
-		try {
-			obj = (JSONObject) parser.parse(new FileReader(source));
-		} catch (FileNotFoundException | ParseException e) {
-		
-			e.printStackTrace();
-		}
-		
-		city.setName((String) ((HashMap<String,Object>) obj).get("name"));
-		city.setCountry((String) ((HashMap<String,Object>) obj).get("country"));
-		JSONObject coord = (JSONObject) ((HashMap<String,Object>) obj).get("coord");
-		city.setLat((String) coord.get("lat"));
-		city.setLon((String) coord.get("lon"));
-
-		
-		
-		LinkedHashSet<City> set = new LinkedHashSet<City>();
-		Iterator<City> it = set.iterator();
-		
-		while (it.hasNext()) {
-			set.add(city);
-			it.next();
+			JSONObject jsonObject = null;
+			try {
+				Object obj = parser.parse(new FileReader(source));
+				jsonObject = (JSONObject) obj;
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			} catch (ParseException e) {
+				
+				e.printStackTrace();
+			}
 			
+					
+	         
+		
+		
+		JSONArray array = (JSONArray) jsonObject.get("array");
+		Iterator<Object> iter = array.iterator();
+     
+		while (iter.hasNext()) {
+			CityWithCountry city = new CityWithCountry();
+			city.setName((String) jsonObject.get("name"));
+			city.setCountry((String) jsonObject.get("country"));
+			JSONObject coord = (JSONObject) jsonObject.get("coord");
+			city.setLat((String) coord.get("lat"));
+			city.setLon((String) coord.getAsString("lon"));
+			
+			cities.add(city);
 		}
 		
-		return (List<City>) set;
+	
+		return cities;
 	}
 
 
 
-	//Da implementare?
-	public List<City> parseIT(File source) {
-		return null;
-	}
-
-	public List<City> parse(File source) {
-		return null;
-	}
 }
